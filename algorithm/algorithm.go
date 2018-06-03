e3bdb2463917574c0870aa2c9dffac4d78fcee61
Cpt Planeta <planetaszymon@gmail.com>
2018-06-03 12:26:02 +0200

    ARMA

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -6,15 +6,24 @@ type Algorithm interface {
 }
 
 type Arma struct {
-
+	data []float64
 }
 
-func (a *Arma) StoreData(data float64) {
+func (a *Arma) StoreData(n float64) {
+	if len(a.data) < 3 {
+		a.data = []float64{n, n, n}
+	} else {
+		a.data = append(a.data[:0], a.data[1:]...)
+		a.data = append(a.data[:], n)
+	}
+}
 
 }
 
 func (a *Arma) Predict() float64 {
-	return 100
+	beta := 0.8
+	gamma := 0.15
+	return (beta*a.data[2]) + (gamma*a.data[1]) + ((1-(beta+gamma))*a.data[0])
 }
 
 func NewArma() *Arma  {
